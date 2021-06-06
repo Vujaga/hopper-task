c19c1b47df79c629b7c43468213d752b3777b3ff
Dejan <vujaga@yahoo.com>
2021-06-06 19:02:22 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const header = document.querySelector('.header');
+const hamburger = document.getElementById('hamburger');
+const nav = document.querySelector('.menu');
+
+// Header changing color on scroll
+window.onscroll = function () {
+  let start = window.scrollY;
+  if (start >= 1) {
+    header.classList.add('onscroll');
+  } else {
+    header.classList.remove('onscroll');
+  }
+};
+
+hamburger.addEventListener('click', function () {
+  nav.classList.toggle('nav-list-mob');
+  header.classList.toggle('header-black');
+});
+
+$('.carousel[data-type="multi"] .item').each(function () {
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  for (var i = 0; i < 3; i++) {
+    next = next.next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
